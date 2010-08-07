5a57131f94ca80c07e340334e32e0ebf8edec0a8
Nathan Rajlich <nathan@tootallnate.net>
2010-08-06 19:12:29 -0700

    Start including basic tests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var connect = require("connect");
 
 module.exports = require("./lib/nTunes").createServer(
-  connect.logger()
+  connect.logger(),
+  connect.staticProvider(__dirname + "/www")
 );
