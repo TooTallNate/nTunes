TooTallNate/nTunes

commit 102ab439481cd753d9f24237101a95212dcaa12d

Fixed decoding problem with '/' and '&' symbols in Specifiers (routing, etc.).

lib/nTunes-specifier.js
@@ -79,7 +79,7 @@ function nSpecifier(specifier) {
             var query = "";
             var keys = Object.keys(filter);
             keys.forEach(function(key, i) {
-              query += key + ' is "' + filter[key] + '"';
+              query += key + ' is "' + decodeURIComponent(filter[key]) + '"';
               if (i < keys.length -1) {
                 query += " and ";
               }

lib/nTunes.js
@@ -129,11 +129,9 @@ nTunes.prototype.handleNTunesRequest = function(req, res, next) {
     isValidApiRequest = false;
   req.parsedUrl = res.parsedUrl = Url.parse(req.url, true);
   
-  req.parsedUrl.decodedPath = decodeURIComponent(req.parsedUrl.pathname);
-  
   // Each "/" in the request URI represents selecting a resource, narrowing
   // down the previous selection, or possibly calling a command.
-  req.parsedUrl.request = req.parsedUrl.decodedPath.split("/").slice(1);
+  req.parsedUrl.request = req.parsedUrl.pathname.split("/").slice(1);
   
   // First check for the case of a "command", since it's only valid with a
   // single token in the URI, and a POST method request.
@@ -155,7 +153,7 @@ nTunes.prototype.handleNTunesRequest = function(req, res, next) {
     // Attempt to resolve the url into a 'specifier'.
     var resolvedSpecifier;
     try {
-      resolvedSpecifier = new nSpecifier(req.parsedUrl.decodedPath);
+      resolvedSpecifier = new nSpecifier(req.parsedUrl.pathname);
     } catch (e) {
       // If ' new nSpecifier' throws an error, then the user requested
       // an invalid specifier. nTunes should respond with a 400 status

commit 7accc30726a2c86e364e36737cf1b6a5cfed717c

Sloppy fix for bug introduced in commit 102ab439481cd753d9f24237101a95212dcaa12d

lib/nTunes-specifier.js
@@ -11,6 +11,9 @@ function nSpecifier(specifier) {
   var i = 0;
   var isList = false;
 
+  // TODO: This parsing logic is a fucking mess. I've been meaning
+  // to rewite it (again) to be more robust. Specifically to fix this
+  // special "/selection/count" case.
   while (i < this.chunks.length) {
     var prevChunk = this.chunks[i-1];
     var next = this.chunks[i++];
@@ -18,25 +21,25 @@ function nSpecifier(specifier) {
       // If the parent chunk was a property, check if it's a class
       // instance, and if it is, set the variable of the previous
       // property request.
-      var parentType = currentObj.getProperty(prevChunk).type;
+      var parentType = currentObj.getProperty(decodeURIComponent(prevChunk)).type;
       if (parentType == 'specifier') {
         isList = true;
         parentType = 'track';
       }
-      var parentClass = nClass.getClass(parentType);
+      var parentClass = nClass.getClass(decodeURIComponent(parentType));
       if (parentClass) {
         currentObj = parentClass;
-        this.defineVar(prevChunk);
+        this.defineVar(decodeURIComponent(prevChunk));
         this._props = [];
       }
     }
-    if (currentObj.hasElement(next)) {
+    if (currentObj.hasElement(decodeURIComponent(next))) {
       // Ensure that
-      this.defineVar('every ' + next);
-      currentObj = nClass.getClass(next);
+      this.defineVar('every ' + decodeURIComponent(next));
+      currentObj = nClass.getClass(decodeURIComponent(next));
     } else {
       // Test for any custom handlers of 'currentObj'
-      var handler = currentObj.getHandler(next);
+      var handler = currentObj.getHandler(decodeURIComponent(next));
       if (handler) {
         this.handler = handler.callback;
       } else {
@@ -44,6 +47,9 @@ function nSpecifier(specifier) {
         // Property names are comma delimited.
         var isPropRequest = true;
         var props = next.split(',');
+        for (var k=0, m=props.length; k<m; k++) {
+          props[k] = decodeURIComponent(props[k]);
+        }
         props.forEach(function(prop) {
           if (!currentObj.hasProperty(prop)) {
             isPropRequest = false;
@@ -53,8 +59,8 @@ function nSpecifier(specifier) {
           this._props = props;
         } else {
           // Test for a number.
-          // Positive is index of parent element.
-          // Negative is the id of the element of parent element.
+          // Positive is index of parent list.
+          // Negative is the id of the element of parent list.
           // Should throw an error if the specifier is not
           // currently parsing inside of a element.
           var numNext = Number(next);
@@ -69,7 +75,7 @@ function nSpecifier(specifier) {
               isList = false;
             } else {
               this._vars.pop();
-              this.defineVar(prevChunk + (numNext < 0 ? ' id':'') + ' ' + Math.abs(numNext));
+              this.defineVar(decodeURIComponent(prevChunk) + (numNext < 0 ? ' id':'') + ' ' + Math.abs(numNext));
             }
           } else if (next.indexOf('=') >= 0) {
             // Test for a query-string style filter. This
@@ -154,7 +160,7 @@ nSpecifier.setNClass = function(nclass) {
 // Accepts an Applescript string like:
 //   'file track id 263 of user playlist id 260 of source id 40 of application "iTunes"'
 // and returns a usable Specifier string:
-//   '/source/-40/playlist/-260/file track/-263'
+//   '/source/-40/user playlist/-260/file track/-263'
 nSpecifier.specifierFromApplescript = function(applescript) {
   var rtn = "";
   applescript.split(" of ").reverse().forEach(function(piece, i) {